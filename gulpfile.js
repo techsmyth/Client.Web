4cc9c479a8552c366860d06c19a4ebc4ba0935c6
Angel <atstojanov@gmail.com>
2020-11-09 12:49:44 +0200

    Add script to generate possible types for graphql

--- gulpfile.js
+++ gulpfile.js
@@ -1,13 +1,15 @@
-require('dotenv').config();
-fs = require('fs');
+const dotenv = require('dotenv');
+const fetch = require('cross-fetch');
+const fs = require('fs');
 
 const CONFIG_TEXT = `window._env_ = `;
 const CONFIG_FILE_NAME = 'env-config.js';
 
 const buildConfiguration = cb => {
+  dotenv.config();
   const env = process.env;
   let configuration = {};
-  const nodeEnv= env.NODE_ENV ? env.NODE_ENV : "development";
+  const nodeEnv = env.NODE_ENV ? env.NODE_ENV : 'development';
 
   console.info(`Building for : '${nodeEnv}'`);
 
@@ -25,5 +27,50 @@ const buildConfiguration = cb => {
   fs.writeFile(`./public/${CONFIG_FILE_NAME}`, `${CONFIG_TEXT}${JSON.stringify(configuration, null, 2)}`, cb);
 };
 
+possibleTypes = cb => {
+  dotenv.config();
+  const endpoint = process.env['REACT_APP_GRAPHQL_ENDPOINT'];
+  console.log('Extracting from: ', endpoint);
+  fetch(endpoint, {
+    method: 'POST',
+    headers: { 'Content-Type': 'application/json' },
+    body: JSON.stringify({
+      variables: {},
+      query: `
+      {
+        __schema {
+          types {
+            kind
+            name
+            possibleTypes {
+              name
+            }
+          }
+        }
+      }
+    `,
+    }),
+  })
+    .then(result => result.json())
+    .then(result => {
+      const possibleTypes = {};
+
+      result.data.__schema.types.forEach(supertype => {
+        if (supertype.possibleTypes) {
+          possibleTypes[supertype.name] = supertype.possibleTypes.map(subtype => subtype.name);
+        }
+      });
+
+      fs.writeFile('./src/generated/possibleTypes.json', JSON.stringify(possibleTypes), err => {
+        if (err) {
+          console.error('Error writing possibleTypes.json', err);
+        } else {
+          console.log('Fragment types successfully extracted!');
+        }
+        cb();
+      });
+    });
+};
+exports.possibleTypes = possibleTypes;
 exports.buildConfiguration = buildConfiguration;
 exports.default = buildConfiguration;
